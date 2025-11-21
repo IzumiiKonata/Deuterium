4d04b92cf44d5d66c9d8444d98c466adc2cb86ee
IzumiiKonata <2030752250@qq.com>
2025-11-21 23:28:42 +0800

    fix: PotionDisplay

--- src/main/java/tritium/widget/impl/PotionDisplay.java
+++ src/main/java/tritium/widget/impl/PotionDisplay.java
@@ -80,6 +80,7 @@ public class PotionDisplay extends Widget {
 
                 GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
                 GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE);
+                GlStateManager.enableTexture2D();
 
                 this.mc.getTextureManager().bindTexture(Location.of("textures/gui/container/inventory.png"));
                 this.drawTexturedModalRect(x + spacing, finalY + spacing, texSize, texSize, iconIndex % 8 * 18, 198 + iconIndex / 8 * 18, 18, 18);
