e299da57d211a0177da6bb98ea26e419dd7bd64c
IzumiiKonata <2030752250@qq.com>
2026-02-14 23:29:58 +0800

    feat: optimize properties

--- tritium/src/main/java/tritium/launch/Launcher.java
+++ tritium/src/main/java/tritium/launch/Launcher.java
@@ -9,12 +9,24 @@ import net.minecraft.launchwrapper.Launch;
 public class Launcher {
 
     public static void main(String[] args) {
-        System.setProperty("tritium.startupTime", String.valueOf(System.currentTimeMillis()));
+        long startTime = System.currentTimeMillis();
+        System.setProperty("tritium.startupTime", String.valueOf(startTime));
 
-        Launch launch = new Launch(args);
+        System.setProperty("java.awt.headless", "true");
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", String.valueOf(Runtime.getRuntime().availableProcessors() - 1));
 
-        System.setProperty("java.net.preferIPv4Stack", "true");
-        launch.launch(args);
+        System.setProperty("log4j2.contextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+        System.setProperty("log4j2.disable.jmx", "true");
+        System.setProperty("log4j2.includeLocation", "false");
+        System.setProperty("log4j2.asyncLoggerRingBufferSize", "262144");
+        System.setProperty("log4j2.asyncLoggerWaitTimeout", "30000");
+
+        System.setProperty("mixin.env.disableRefMap", "true");
+        System.setProperty("mixin.env.remapRefMap", "false");
+        System.setProperty("mixin.debug.verbose", "false");
+        System.setProperty("mixin.debug.export", "false");
+
+        new Launch(args).launch(args);
     }
 
 }
