9205a22a4418e3cd4e18e62cbf49fa73f690c39b
IzumiiKonata <2030752250@qq.com>
2026-02-16 14:21:29 +0800

    feat: optimizations

--- tritium/src/main/java/net/minecraft/util/ObjectIntIdentityMap.java
+++ tritium/src/main/java/net/minecraft/util/ObjectIntIdentityMap.java
@@ -1,37 +1,34 @@
 package net.minecraft.util;
 
-import com.google.common.base.Predicates;
-import com.google.common.collect.Iterators;
-import com.google.common.collect.Lists;
+import it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
-import java.util.IdentityHashMap;
 import java.util.Iterator;
-import java.util.List;
 
 public class ObjectIntIdentityMap<T> implements IObjectIntIterable<T> {
-    private final IdentityHashMap<T, Integer> identityMap = new IdentityHashMap<>(512);
-    private final List<T> objectList = Lists.newArrayList();
+    private final Reference2IntOpenHashMap<T> identityMap;
+    private final Int2ObjectOpenHashMap<T> valueMap;
+
+    public ObjectIntIdentityMap() {
+        this.identityMap = new Reference2IntOpenHashMap<>(512);
+        this.valueMap = new Int2ObjectOpenHashMap<>(512);
+        this.identityMap.defaultReturnValue(-1);
+    }
 
     public void put(T key, int value) {
         this.identityMap.put(key, value);
-
-        while (this.objectList.size() <= value) {
-            this.objectList.add(null);
-        }
-
-        this.objectList.set(value, key);
+        this.valueMap.put(value, key);
     }
 
     public int get(T key) {
-        Integer integer = this.identityMap.get(key);
-        return integer == null ? -1 : integer;
+        return this.identityMap.getInt(key);
     }
 
     public final T getByValue(int value) {
-        return value >= 0 && value < this.objectList.size() ? this.objectList.get(value) : null;
+        return this.valueMap.get(value);
     }
 
     public Iterator<T> iterator() {
-        return Iterators.filter(this.objectList.iterator(), Predicates.notNull());
+        return this.valueMap.values().iterator();
     }
 }
