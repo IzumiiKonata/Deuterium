3b0819024f7a65abb20a9d6eb5326aebb94e68f5
IzumiiKonata <2030752250@qq.com>
2026-02-17 21:45:56 +0800

    fix(ObjectIntIdentityMap): 修复 Map 内容无顺序的问题

--- tritium/src/main/java/net/minecraft/util/ObjectIntIdentityMap.java
+++ tritium/src/main/java/net/minecraft/util/ObjectIntIdentityMap.java
@@ -1,17 +1,19 @@
 package net.minecraft.util;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Reference2IntLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
 import java.util.Iterator;
 
 public class ObjectIntIdentityMap<T> implements IObjectIntIterable<T> {
-    private final Reference2IntOpenHashMap<T> identityMap;
-    private final Int2ObjectOpenHashMap<T> valueMap;
+    private final Reference2IntLinkedOpenHashMap<T> identityMap;
+    private final Int2ObjectLinkedOpenHashMap<T> valueMap;
 
     public ObjectIntIdentityMap() {
-        this.identityMap = new Reference2IntOpenHashMap<>(512);
-        this.valueMap = new Int2ObjectOpenHashMap<>(512);
+        this.identityMap = new Reference2IntLinkedOpenHashMap<>(512);
+        this.valueMap = new Int2ObjectLinkedOpenHashMap<>(512);
         this.identityMap.defaultReturnValue(-1);
     }
 
